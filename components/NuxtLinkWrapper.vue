c36f22f9d2518ffb1795308011514b8f350a4adb
Sergey <azrailomgpro@gmail.com>
2020-10-14 15:28:55 +0300

    bug repro

---
+++ components/NuxtLinkWrapper.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    style="display: flex; flex-flow: column;"
+  >
+    <n-link :to="to" :href="href">
+      value taken from prop (not set)
+    </n-link>
+    <n-link :to="to" :href="undefined">
+      undefined
+    </n-link>
+    <n-link :to="to">
+      no :href passed to n-link
+    </n-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: ['to', 'href']
+})
+</script>
